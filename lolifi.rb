1699681b706b117eae6c5c2c7a729e3011538d1d
usok <60635852+usokk@users.noreply.github.com>
2020-12-26 23:35:27 -0300

    Update lolifi.rb

--- lolifi.rb
+++ lolifi.rb
@@ -13,14 +13,6 @@ banner = "
 "
 print banner.bold.blue
 
-class String
-def red;            "\e[31m#{self}\e[0m" end
-def green;          "\e[32m#{self}\e[0m" end
-def blue;           "\e[34m#{self}\e[0m" end
-def bold;           "\e[1m#{self}\e[22m" end
-def underline;      "\e[4m#{self}\e[24m" end
-end
-
 puts " " 
 
 print " TARGET: ".yellow
